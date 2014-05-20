78344a42fa88378bf510a070a62b627758d99485
Robert Pelger <rpelger1004@gmail.com>
2014-05-20 00:21:36 -0400

    switching branches

---
+++ src/MCBlockFinder/MCBlockFinder/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MCBlockFinder
+//
+//  Created by Robert Pelger on 5/19/14.
+//  Copyright (c) 2014 MidnightBadgers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
