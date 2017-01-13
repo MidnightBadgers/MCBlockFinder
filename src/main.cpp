35d4aed5fdc86e0bcf211a338fdbb6984ea9a9cd
unrealcroissant <rpelger1004@gmail.com>
2017-01-13 11:23:24 +0000

    Added boilerplate main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by unrealcroissant on 1/13/17.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Goodbye!";
+    return 0;
+
+}
